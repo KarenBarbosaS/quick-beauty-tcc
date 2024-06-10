dbc94e9fe4ff819ef58a6eb4d6c13ad08c8a81ba
KarenBarbosaS <karenbarbosas123@gmail.com>
2024-06-10 14:39:07 -0300

    refactor

--- src/pages/RegisterPage/register.tsx
+++ src/pages/RegisterPage/register.tsx
@@ -7,6 +7,7 @@ import FormContainer from "../../components/Forms/FormContainer/form-container";
 import ImageUpload from "../../components/UploadImage/upload-image";
 import { applyMask, masks } from "../../utils/maskUtils";
 import * as S from "./register-styled";
+import { toast } from "react-toastify";
 
 const FormPage: React.FC = () => {
   const [formData, setFormData] = useState({
@@ -19,6 +20,7 @@ const FormPage: React.FC = () => {
     phoneNumber: "",
     cep: "",
     houseNumber: "",
+    evaluationNote: "0.0",
   });
   const [profileImage, setProfileImage] = useState<File | null>(null);
   const [errors, setErrors] = useState<string[]>([]);
@@ -57,6 +59,7 @@ const FormPage: React.FC = () => {
       houseNumber,
     } = formData;
 
+    // if (!userImage) newErrors.push('Imagem é obrigatória');
     if (!username) newErrors.push("Nome é obrigatório");
     if (!cpf || cpf.length < 14) newErrors.push("CPF inválido");
     if (!dateOfBirth) newErrors.push("Data de nascimento é obrigatória");
@@ -89,8 +92,11 @@ const FormPage: React.FC = () => {
       form.append("phoneNumber", formData.phoneNumber);
       form.append("cep", formData.cep);
       form.append("houseNumber", formData.houseNumber);
+      form.append("evaluationNote", formData.evaluationNote);
       if (profileImage) {
-        form.append("profileImage", profileImage);
+        form.append("userImage", profileImage);
+      } else {
+        form.append("userImage", "");
       }
 
       const response = await fetch(
@@ -106,11 +112,11 @@ const FormPage: React.FC = () => {
       }
 
       const data = await response.json();
-      alert("Usuário criado com sucesso!");
+      toast.success("Usuário criado com sucesso!");
       console.log(data);
     } catch (error) {
       console.error("Erro ao criar usuário:", error);
-      alert("Erro ao criar usuário. Por favor, tente novamente.");
+      toast.error("Erro ao criar usuário. Por favor, tente novamente.");
     }
   };
 
@@ -127,89 +133,78 @@ const FormPage: React.FC = () => {
         </ul>
       )}
       <ImageUpload onImageSelect={handleImageSelect} />
-      <S.Content>
-        <InputField
-          label="Nome"
-          id="username"
-          type="text"
-          placeholder="Digite seu nome"
-          onChange={handleChange}
-          className="name-input input"
-        />
-        <InputField
-          label="CPF"
-          id="cpf"
-          type="text"
-          placeholder="Digite seu CPF"
-          onChange={handleChange}
-          value={formData.cpf}
-          className="cpf-input input"
-        />
-        <InputField
-          label="Data de Nascimento"
-          id="dateOfBirth"
-          type="date"
-          onChange={handleChange}
-          value={formData.dateOfBirth}
-          className="birth-input input"
-        />
-        <InputField
-          label="E-mail"
-          id="email"
-          type="email"
-          placeholder="Digite seu e-mail"
-          onChange={handleChange}
-          value={formData.email}
-          className="email-input input"
-        />
-        <InputField
-          label="Senha"
-          id="password"
-          type="password"
-          placeholder="Digite sua senha"
-          onChange={handleChange}
-          value={formData.password}
-          className="password-input input"
-        />
-        <SelectField
-          label="Tipo de Usuário"
-          id="type"
-          options={[
-            { value: "Cliente", label: "Cliente" },
-            { value: "Professional", label: "Profissional" },
-          ]}
-          onChange={handleChange}
-          value={formData.type}
-          className="type-select select"
-        />
-        <InputField
-          label="Telefone"
-          id="phoneNumber"
-          type="text"
-          placeholder="Digite seu telefone"
-          onChange={handleChange}
-          value={formData.phoneNumber}
-          className="tel-input input"
-        />
-        <InputField
-          label="CEP"
-          id="cep"
-          type="text"
-          placeholder="Digite seu CEP"
-          onChange={handleChange}
-          value={formData.cep}
-          className="cep-input input"
-        />
-        <InputField
-          label="Número da Casa"
-          id="houseNumber"
-          type="text"
-          placeholder="Digite o número da casa"
-          onChange={handleChange}
-          value={formData.houseNumber}
-          className="number-input input"
-        />
-      </S.Content>
+      <InputField
+        label="Nome"
+        id="username"
+        type="text"
+        placeholder="Digite seu nome"
+        onChange={handleChange}
+      />
+      <InputField
+        label="CPF"
+        id="cpf"
+        type="text"
+        placeholder="Digite seu CPF"
+        onChange={handleChange}
+        value={formData.cpf}
+      />
+      <InputField
+        label="Data de Nascimento"
+        id="dateOfBirth"
+        type="date"
+        onChange={handleChange}
+        value={formData.dateOfBirth}
+      />
+      <InputField
+        label="E-mail"
+        id="email"
+        type="email"
+        placeholder="Digite seu e-mail"
+        onChange={handleChange}
+        value={formData.email}
+      />
+      <InputField
+        label="Senha"
+        id="password"
+        type="password"
+        placeholder="Digite sua senha"
+        onChange={handleChange}
+        value={formData.password}
+      />
+      <SelectField
+        label="Tipo de Usuário"
+        id="type"
+        options={[
+          { value: "Cliente", label: "Cliente" },
+          { value: "Professional", label: "Profissional" },
+        ]}
+        onChange={handleChange}
+        value={formData.type}
+      />
+      <InputField
+        label="Telefone"
+        id="phoneNumber"
+        type="text"
+        placeholder="Digite seu telefone"
+        onChange={handleChange}
+        value={formData.phoneNumber}
+      />
+      <InputField
+        label="CEP"
+        id="cep"
+        type="text"
+        placeholder="Digite seu CEP"
+        onChange={handleChange}
+        value={formData.cep}
+      />
+      <InputField
+        label="Número da Casa"
+        id="houseNumber"
+        type="text"
+        placeholder="Digite o número da casa"
+        onChange={handleChange}
+        value={formData.houseNumber}
+      />
       <Button label="Criar" onClick={handleSubmit} />
     </FormContainer>
   );
